aea7d999ad9c5e6d1cbb50fd5585bad9557f36cb
Daniil <ckiper@yandex.ru>
2024-01-29 15:39:24 +0100

    add logs for load_from_api

--- src/load_from_api.rs
+++ src/load_from_api.rs
@@ -8,6 +8,7 @@ pub async fn load_from_api(rocksdb_client: &RocksdbClient, from_timestamp: u32, 
     let mut is_processed = false;
 
     log::info!("start load from api");
+    let mut count_inserted = 0;
     let mut from_timestamp_request = from_timestamp;
     let mut last_timestamp_lt = 0;
     loop {
@@ -30,21 +31,50 @@ pub async fn load_from_api(rocksdb_client: &RocksdbClient, from_timestamp: u32, 
             }
         }
 
+        log::info!("insert transactions len {}", transactions.len());
+        count_inserted += transactions.len();
         rocksdb_client.insert_transactions_with_drain(&mut transactions);
+        log::info!("count inserted transactions {}", count_inserted);
     }
 
     Ok(())
 }
 
 #[cfg(test)]
+fn prepare_test(level_filter: log::LevelFilter) -> () {
+    use env_logger::Builder;
+    use std::io::Write;
+    use std::str::FromStr;
+
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} {}/{} {} [{}] - {}",
+                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.module_path().unwrap_or_default(),
+                record.file().unwrap_or_default(),
+                record.line().unwrap_or_default(),
+                record.level(),
+                record.args(),
+            )
+        })
+        .filter(Some("transaction_buffer"), level_filter)
+        .init();
+}
+
+#[cfg(test)]
 mod test {
     use std::sync::Arc;
+    use crate::load_from_api::prepare_test;
 
     use crate::models::RocksdbClientConstants;
     use crate::utils::create_rocksdb;
 
     #[tokio::test]
     async fn test_load_from_api() {
+        prepare_test(log::LevelFilter::Debug);
+        println!("test_load_from_api");
         let rocksdb = Arc::new(create_rocksdb(
             "./raw_transactions",
             RocksdbClientConstants {
@@ -54,7 +84,8 @@ mod test {
             },
         ));
 
-        super::load_from_api(&rocksdb, 0, "", "").await.unwrap();
+        println!("load_from_api");
+        super::load_from_api(&rocksdb, 1705760950, "", "").await.unwrap();
         println!("{}", rocksdb.count_not_processed_transactions());
         drop(rocksdb);
         std::fs::remove_dir_all("./raw_transactions").unwrap();
